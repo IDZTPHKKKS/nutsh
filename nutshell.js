309b3de1266c5df9761d43398eaca0f789da198c
IDZTPHKKKS <shyaamchess@gmail.com>
2023-05-13 21:43:19 +0530

    Added files

---
+++ nutshell.js
@@ -0,0 +1,16 @@
+function product()
+{
+    window.location.assign("index.html");
+}
+function about()
+{
+    window.location.assign("about.html");
+}
+function team()
+{
+    window.location.assign("team.html");
+}
+function contact()
+{
+    window.location.assign("contact.html");
+}
